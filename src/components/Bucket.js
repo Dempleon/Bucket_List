6ee261f0815a8f1daf825f84619b49d3c7b182e1
Dempoleon <dempoleon@gmail.com>
2022-12-01 14:56:10 -0800

    it works

---
+++ src/components/Bucket.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import BucketForm from './BucketForm';
+
+function Bucket(props) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+    eagerness: '',
+  });
+
+  console.log(props.bucket);
+
+  const submitUpdate = (value) => {
+
+    // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
+
+    props.editBucketItem(edit.id, value);
+    // TODO: Set the key:value pairs in the `edit` object back to empty strings
+    setEdit({id: null, value: '', eagerness: ''});
+
+  };
+
+  // If the user is attempting to edit an item, render the bucket form with the edit variable passed as a prop
+  if (edit.id) {
+    return <BucketForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return props.bucket.map((item, index) => (
+
+
+    <div className={`bucket row ${item.eagerness}`} key={item.id}>
+
+      {/*  TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
+      <div key={item.id} onClick={() => props.completeBucketItem(item.id)}>
+          {/* TODO: Add the item text here */}
+          {item.text}
+      </div>
+      <div className="icons">
+        {/*  TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
+        <p onClick={() => setEdit({id:item.id, value: item.text, eagerness: item.eagerness})}> ✏️</p>
+        {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
+        
+        <p onClick={() => {props.removeBucketItem(item.id)}}> 🗑️</p>
+      </div>
+    </div>
+  ));
+}
+
+export default Bucket;
